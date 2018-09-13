c08122ea76512b180df15f4cea5b76fcaec69fc6
Ethel C <cofresi.ethel@gmail.com>
2018-09-13 12:36:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
+11 lines (8 sloc)  126 Bytes
 class Application < Sinatra::Base
-  # Write your code here!
-
+  get '/' do		
+    erb :index		
+  end		
+  
+  post '/greet' do		
+    erb :greet		
+  end
 end
